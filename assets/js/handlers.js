aea686c22a1c32a22b50b05967ffbfe3af3a3241
Gabriel Luz <gabrielluzsantosl@gmail.com>
2022-03-16 23:10:30 -0300

    criando os itens do carrinho e parte das informações do carrinho, deixando o site responsivo, criando a feature de modo escuro e claro

---
+++ assets/js/handlers.js
@@ -0,0 +1,18 @@
+// function claroEscuro() {
+//     let blackStyle = document.getElementById('modo');
+//     if (!blackStyle) {
+//         let head = document.querySelector('head');
+//         let link = document.createElement('link');
+//         link.setAttribute('href', 'assets/css/indexBlack.css');
+//         link.setAttribute('rel', 'stylesheet');
+//         link.setAttribute('id', 'modo');
+//         head.appendChild(link);
+
+//         luminosidade.setAttribute('src', 'assets/images/sol.png')
+//     } else {
+//         let head = document.querySelector('head');
+//         head.removeChild(blackStyle);
+
+//         luminosidade.setAttribute('src', 'assets/images/lua.png')
+//     }
+// }
---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+// ao colocar o carrinho na frente vou precisar mudar essas propriedades:
+
+// // .cart {
+// //     left: 0vw;
+// //     display: flex;
+// //     position: absolute;
+// //     top: 0;
+// //     height: 100vh;
+// // }
+
+//    main {
+//        display:none;
+//    }
+
+// let luminosidade = document.querySelector('header > img');
+
+// luminosidade.addEventListener('click', claroEscuro)
+
+// console.log(luminosidade)
