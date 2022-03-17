20cb228716cebf5314c0a5407ffcdb57cdd4376d
Gabriel Luz <gabrielluzsantosl@gmail.com>
2022-03-17 09:48:19 -0300

    criando o array de objetos que vai ter as informações dos produtos

--- assets/js/produtos.js
+++ assets/js/produtos.js
@@ -1,3 +1,43 @@
-// let produtos = [
-//     {}
-// ]
+let produtos = [{
+        img: 'assets/images/jaqueta.png',
+        categoria: 'Camisetas',
+        nome: 'Lightweight Jacket',
+        descricao: 'Adicione um pouco de energia ao seu guarda-roupa de inverno com esta jaqueta vibrante...',
+        preco: 100
+    },
+    {
+        img: 'assets/images/gorro.png',
+        categoria: 'Acessórios',
+        nome: 'Black Hat',
+        descricao: 'O gorro Next.js chegou! Esta beldade bordada tem um ajuste confortável que garante que...',
+        preco: 100
+    },
+    {
+        img: 'assets/images/mascara.png',
+        categoria: 'Acessórios',
+        nome: 'Mask',
+        descricao: 'Esta máscara facial durável é feita de duas camadas de tecido tratado e possui presilhas...',
+        preco: 40
+    },
+    {
+        img: 'assets/images/camisa.png',
+        categoria: 'Camisetas',
+        nome: 'T-Shirt',
+        descricao: 'Esta t-shirt é imprescindível no seu guarda-roupa, combinando o caimento intemporal de...',
+        preco: 100
+    },
+    {
+        img: 'assets/images/camiseta.png',
+        categoria: 'Camisetas',
+        nome: 'Short-Sleeve T-Shirt',
+        descricao: 'Agora você encontrou a camiseta básica do seu guarda-roupa. É feito de um mais grosso...',
+        preco: 100
+    },
+    {
+        img: 'assets/images/casaco.png',
+        categoria: 'Camisetas',
+        nome: 'Champion Packable Jacket',
+        descricao: 'Proteja-se dos elementos com esta jaqueta embalável Champion. Esta jaqueta de poliést...',
+        preco: 100
+    },
+]
